d763c8536a533e23b6538bd1c361782b22f76c4d
Huckleberry Febbo <febbo@umich.edu>
2018-03-04 16:55:05 -0500

    finished first complete working version with multiple obstacles being parametrically loaded including speed

--- ros/src/system/gazebo/mavs/mavs_gazebo/move_obstacles.jl
+++ ros/src/system/gazebo/mavs/mavs_gazebo/move_obstacles.jl
@@ -9,34 +9,45 @@ import geometry_msgs.msg: Twist
 
 # TODO
 # 1) check to see if model is paused
-# 2) pass config file names
 
-function loop(pub, obs_num)
+"""
+--------------------------------------------------------------------------------------\n
+Author: Huckleberry Febbo, Graduate Student, University of Michigan
+Date Create: 2/28/2018, Last Modified: 2/28/2018 \n
+--------------------------------------------------------------------------------------\n
+"""
+function loop(pub)
+    robotNamespace = RobotOS.get_param("robotNamespace")
+    obs_num = length(RobotOS.get_param(string(robotNamespace,"/obs/radius")))
+
     loop_rate = Rate(5.0)
 
-    #modelName = RobotOS.get_param("obstacle_name")
-    modelName = "obstacle"
-
-    RobotOS.set_param("mavs/bool/init_move_obstacles",true)
+    RobotOS.set_param(string(robotNamespace,"/bool/init_move_obstacles"),true)
+    println("obstacle plugin in julia has been initialized.")
 
     while !is_shutdown()
-
         for i in 1:obs_num
             cmd = Twist()
-            cmd.linear.x = RobotOS.get_param("mavs/obs/vx")[i]
-            cmd.linear.y = RobotOS.get_param("mavs/obs/vy")[i]
+            cmd.linear.x = RobotOS.get_param(string(robotNamespace,"/obs/vx"))[i]
+            cmd.linear.y = RobotOS.get_param(string(robotNamespace,"/obs/vy"))[i]
             publish(pub[i], cmd)
         end
         rossleep(loop_rate)
     end
 end
 
+"""
+--------------------------------------------------------------------------------------\n
+Author: Huckleberry Febbo, Graduate Student, University of Michigan
+Date Create: 2/28/2018, Last Modified: 2/28/2018 \n
+--------------------------------------------------------------------------------------\n
+"""
 function main()
     init_node("rosjl_move_obstacles")
+    robotNamespace = RobotOS.get_param("robotNamespace")
+    obs_num = length(RobotOS.get_param(string(robotNamespace,"/obs/radius")))
 
-    RobotOS.set_param("mavs/bool/init_move_obstacles",false)
-
-    obs_num = RobotOS.get_param("mavs/obs/num")
+    RobotOS.set_param(string(robotNamespace,"/bool/init_move_obstacles"),false)
 
     pub = Array{Publisher{Twist}}(obs_num)
 
@@ -44,7 +55,7 @@ function main()
         pub[i] = Publisher{Twist}(string("Obstacle",i,"/cmd_vel"), queue_size = 10)
     end
 
-    loop(pub, obs_num)
+    loop(pub)
 end
 
 if ! isinteractive()
--- ros/src/system/gazebo/mavs/mavs_gazebo/urdf/launch_obstacles.py
+++
@@ -1,8 +0,0 @@
-import roslaunch
-package = 'mavs_gazebo'
-executable = 'YOUR_SCRIPT.py'
-node_name = 'YOUR_NODE_NAME'
-node = roslaunch.core.Node(package=package, node_type=executable, name=node_name)
-launch = roslaunch.scriptapi.ROSLaunch()
-launch.start()
-process = launch.launch(node)

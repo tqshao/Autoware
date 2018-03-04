0e02b49c402d2c74d6351d8a99ae8fcd08b9dbea
Huckleberry Febbo <febbo@umich.edu>
2018-03-04 11:35:53 -0500

    setting obstacle speed parametrically

--- ros/src/system/gazebo/mavs/mavs_gazebo/move_obstacles.jl
+++ ros/src/system/gazebo/mavs/mavs_gazebo/move_obstacles.jl
@@ -10,19 +10,22 @@ import geometry_msgs.msg: Twist
 # TODO
 # 1) check to see if model is paused
 
-function loop(pub)
+function loop(pub, obs_num)
     loop_rate = Rate(5.0)
 
     #modelName = RobotOS.get_param("obstacle_name")
-    modelName = "obstacles"
+    modelName = "obstacle_0"
+
+    RobotOS.set_param("mavs/bool/init_move_obstacles",true)
 
     while !is_shutdown()
 
-        cmd = Twist()
-        cmd.linear.x = 10
-       println("Calling 'gazebo/set_model_state' service...")
-
-        publish(pub, cmd)
+        for i in 1:obs_num
+            cmd = Twist()
+            cmd.linear.x = RobotOS.get_param("mavs/obs/vx")[i]
+            cmd.linear.y = RobotOS.get_param("mavs/obs/vy")[i]
+            publish(pub[i], cmd)
+        end
         rossleep(loop_rate)
     end
 end
@@ -30,12 +33,17 @@ end
 function main()
     init_node("rosjl_move_obstacles")
 
-    RobotOS.set_param("mavs/bool/init_move_obstalces",false)
+    RobotOS.set_param("mavs/bool/init_move_obstacles",false)
 
-    pub = Publisher{Twist}("cmd_vel", queue_size = 10)
+    obs_num = RobotOS.get_param("mavs/obs/num")
 
+    pub = Array{Publisher{Twist}}(obs_num)
 
-    loop(pub)
+    for i in 1:obs_num
+        pub[i] = Publisher{Twist}(string("obstacle/cmd_vel_",i-1), queue_size = 10)
+    end
+
+    loop(pub, obs_num)
 end
 
 if ! isinteractive()
